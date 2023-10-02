1df8539af231d95cb241e88907c14faab0ffc28a
Blaize Egelhof <Blaize.Egelhof@outlook.com>
2023-10-02 14:38:08 +0000

    fix!

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "HamBurgers")
-os.environ.setdefault("DEBUG", "True")
-os.environ.setdefault("DEVELOPMENT", "True")
-os.environ.setdefault("DB_URL", "postgresql:///taskmanager")
-os.environ.setdefault("DATABASE_URL", "postgres://zfhuutds:GDNiI3l1nvj_1dwNpoyiPoBsDn1Cy0NT@silly.db.elephantsql.com/zfhuutds")
